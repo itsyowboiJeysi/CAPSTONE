130a616d9c29ab0e0d5377c88bf6b5ea26e5b064
John Christian Castro <jcortsac123@gmail.com>
2026-04-26 11:29:06 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');         // tell Express to use EJS
+app.set('views', './views');           // folder where your .ejs files live
+app.use(express.static('public'));     // serve CSS/images from /public
+
+app.get('/', (req, res) => {
+  res.render('login');                 // renders views/login.ejs
+});
+
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+  });
+
+
+app.get('/monitoring', (req, res) => {
+    res.render('realtime-monitoring');
+  });
+  
+app.get('/water-quality', (req, res) => {
+    res.render('water-quality');
+  });
+
+  app.get('/consumption', (req, res) => {
+    res.render('consumption');
+  });
+
+  app.get('/alerts', (req, res) => {
+    res.render('alerts');
+  });
+
+  app.get('/history', (req, res) => {
+    res.render('history');
+  });
+app.use(express.urlencoded({ extended: true })); // parse form data
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  if (email === 'admin@etubig.com' && password === 'secret123') {
+    res.redirect('/dashboard');
+  } else {
+    res.render('login', { error: 'Invalid credentials' });
+  }
+});
+app.listen(3000);
